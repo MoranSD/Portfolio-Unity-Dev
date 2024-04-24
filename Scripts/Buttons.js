3a36aa236bf1bd89980be63e9dfe079a6061479a
MoranSD <93720897+MoranSD@users.noreply.github.com>
2024-04-25 02:46:55 +0300

    фыв

--- Scripts/Buttons.js
+++ Scripts/Buttons.js
@@ -51,7 +51,7 @@ const projectsArray = [
   },
   {
     name: 'Fat Jump',
-    videoLink: '<https://www.youtube.com/embed/UwzvTpLJN9A?si=2QjLUkl30q1KXlti',
+    videoLink: '<https://www.youtube.com/embed/UwzvTpLJN9A',
     description: 'Заказ, исполнял обязанности программиста, написание механик, работа с анимацией/эффектами, оптимизация.'
   },
   {
